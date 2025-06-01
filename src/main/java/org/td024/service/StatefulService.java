49a05a35f9d527b46d61c6a3cca31608923639d4
TheDev024 <thedeveloper024@gmail.com>
2025-06-01 20:07:23 +0400

    Removed redundant declaration of fis & fos; switched to java.nio from java.io for FileInputStream and FileOutputStream
    Signed-off-by: TheDev024 <thedeveloper024@gmail.com>

--- src/main/java/org/td024/service/StatefulService.java
+++ src/main/java/org/td024/service/StatefulService.java
@@ -4,7 +4,11 @@ import org.td024.entity.Entity;
 import org.td024.exception.ClassChangedException;
 import org.td024.exception.StateFileNotFoundException;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 public abstract class StatefulService<T extends Entity> {
@@ -19,15 +23,15 @@ public abstract class StatefulService<T extends Entity> {
     protected abstract void setData(List<T> data);
 
     public void saveState() throws StateFileNotFoundException {
-        try (FileOutputStream fos = new FileOutputStream(getFilePath()); ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(Files.newOutputStream(Paths.get(getFilePath())))) {
             oos.writeObject(getData());
         } catch (IOException e) {
-            throw new StateFileNotFoundException("File not found to load state: " + getFilePath());
+            throw new StateFileNotFoundException("File not found to save state: " + getFilePath());
         }
     }
 
     public void loadState() throws ClassChangedException, StateFileNotFoundException {
-        try (FileInputStream fis = new FileInputStream(getFilePath()); ObjectInputStream ois = new ObjectInputStream(fis)) {
+        try (ObjectInputStream ois = new ObjectInputStream(Files.newInputStream(Paths.get(getFilePath())))) {
             List<T> loadedData = (List<T>) ois.readObject();
             setData(loadedData);
             int lastId = loadedData.stream().mapToInt(T::getId).max().orElse(0);
