5c752be2dfb6069b1f48fa70345bc488540cf494
TheDev024 <thedeveloper024@gmail.com>
2025-06-18 17:52:59 +0400

    test: Add unit tests for WorkspaceRepository save() function
    Signed-off-by: TheDev024 <thedeveloper024@gmail.com>

---
+++ src/test/java/org/td024/dao/WorkspaceRepositoryTest.java
@@ -0,0 +1,118 @@
+package org.td024.dao;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.td024.entity.Workspace;
+import org.td024.enums.WorkspaceType;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class WorkspaceRepositoryTest {
+    private WorkspaceRepository repository;
+
+    @BeforeEach
+    public void setUp() {
+        repository = new WorkspaceRepository();
+        repository.setData(new ArrayList<>());
+    }
+
+    @Test
+    void saveShouldReturnId() {
+        // Arrange
+        Workspace workspace1 = new Workspace("Test Workspace 1", WorkspaceType.OPEN, BigDecimal.ONE);
+        Workspace workspace2 = new Workspace("Test Workspace 2", WorkspaceType.OPEN, BigDecimal.ONE);
+        Workspace workspace3 = new Workspace("Test Workspace 3", WorkspaceType.OPEN, BigDecimal.ONE);
+
+        // Act
+        int id1 = repository.save(workspace1);
+        int id2 = repository.save(workspace2);
+        int id3 = repository.save(workspace3);
+
+        // Assert
+        assertEquals(1, id1);
+        assertEquals(2, id2);
+        assertEquals(3, id3);
+    }
+
+    @Test
+    void saveShouldAssignWorkspaceId() {
+        // Arrange
+        Workspace workspace1 = new Workspace("Test Workspace 1", WorkspaceType.OPEN, BigDecimal.ONE);
+        Workspace workspace2 = new Workspace("Test Workspace 2", WorkspaceType.OPEN, BigDecimal.ONE);
+        Workspace workspace3 = new Workspace("Test Workspace 3", WorkspaceType.OPEN, BigDecimal.ONE);
+
+        // Act
+        repository.save(workspace1);
+        repository.save(workspace2);
+        repository.save(workspace3);
+
+        // Assert
+        assertEquals(1, workspace1.getId());
+        assertEquals(2, workspace2.getId());
+        assertEquals(3, workspace3.getId());
+    }
+
+    @Test
+    void saveShouldEditWorkspaceById() {
+        // Arrange
+        Workspace workspace = new Workspace("Test Workspace", WorkspaceType.OPEN, BigDecimal.ONE);
+        int id = repository.save(workspace);
+        workspace = new Workspace(id, "Edited Workspace", WorkspaceType.OPEN, BigDecimal.ONE);
+
+        // Act
+        repository.save(workspace);
+
+        // Assert
+        repository.getWorkspaceById(id).ifPresentOrElse(edited -> assertEquals("Edited Workspace", edited.getName()), () -> {
+        });
+    }
+
+    @Test
+    void saveShouldReturnSameIdForEdit() {
+        // Arrange
+        Workspace workspace = new Workspace("Test Workspace", WorkspaceType.OPEN, BigDecimal.ONE);
+        int id = repository.save(workspace);
+        workspace = new Workspace(id, "Edited Workspace", WorkspaceType.OPEN, BigDecimal.ONE);
+
+        // Act
+        int editedId = repository.save(workspace);
+
+        // Assert
+        assertEquals(id, editedId);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 1, 10, Integer.MAX_VALUE, Integer.MIN_VALUE})
+    void shouldReturnNegative1IfUnsuccessful(int id) {
+        // Arrange
+        Workspace workspace = new Workspace(id, "Test Workspace", WorkspaceType.OPEN, BigDecimal.ONE);
+
+        // Act
+        id = repository.save(workspace);
+
+        // Assert
+        assertEquals(-1, id);
+    }
+
+    @Test
+    void deletedWorkspaceEditShouldBeUnsuccessful() {
+        // Arrange
+        Workspace workspace = new Workspace("Test Workspace", WorkspaceType.OPEN, BigDecimal.ONE);
+        int id = repository.save(workspace);
+        repository.deleteWorkspace(id);
+        workspace = new Workspace(id, "Edited Workspace", WorkspaceType.OPEN, BigDecimal.ONE);
+
+        // Act
+        int editedId = repository.save(workspace);
+
+        // Assert
+        assertEquals(-1, editedId);
+    }
+
+
+}
